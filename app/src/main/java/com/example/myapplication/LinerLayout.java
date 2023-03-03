a6aaff199c1d326e5f9e27e069d31b4b3ad17b2a
Zolomed <sasha.zolotarev10@gmail.com>
2023-03-04 00:23:22 +0300

    Performed initialization

--- app/src/main/java/com/example/myapplication/LinerLayout.java
+++ app/src/main/java/com/example/myapplication/LinerLayout.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class LinerLayout extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class LinerLayout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.liner_layout);
+        EditText editText = (EditText) findViewById(R.id.editTextGrup1);
+        editText.setText(getString(R.string.hint_text));
+        ImageView image = findViewById(R.id.imageView1);
+        image.setImageResource(R.drawable.pr3);
     }
 }
