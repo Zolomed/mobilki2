917858bc85e249168b07b1815d0a570fe3de6177
Zolomed <sasha.zolotarev10@gmail.com>
2023-03-10 23:14:14 +0300

    Redid the log

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
+    private static final String TAG = "Moi Dnewnik";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.authorizationButton).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Log.i(TAG, "Сработал log");
+            }
+        });
     }
 }
